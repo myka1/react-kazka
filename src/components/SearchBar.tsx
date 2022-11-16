d5dc0d3498bc5949ab7a59075a2d9005050e13b8
myka1 <msmykolas@gmail.com>
2022-11-16 21:25:02 +0200

    adding search by last name

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,9 +2,7 @@ const SearchBar = ({ onChange }: { onChange: (query: string) => void }) => {
   return (
     <div>
       <input placeholder="Search" onChange={(e) => onChange(e.target.value)} />
-      <span className="searchBar" style={{ textShadow: "0.1px 0.5px" }}>
-        Search Bar
-      </span>
+      <span className="searchBar">Search Bar</span>
     </div>
   );
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
 import { User } from "./App";
 
 export const getFilteredUsers = (users: User[], searchQuery: string) => {
-    const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+    const filteredUsers = users.filter((user) => {
+      return  user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              user.lastName.toLowerCase().includes(searchQuery.toLowerCase())
+    } );
     return filteredUsers;
 }
